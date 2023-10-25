d78f7176ce8f38b2403af0aeb8aeec55dbf26bff
Davi Siqueira <davi.mlp@gmail.com>
2023-10-25 19:15:37 +0100

    libft

--- Makefile
+++ Makefile
@@ -6,39 +6,37 @@
 #    By: davioliv <marvin@42.fr>                    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/04/17 10:03:20 by davioliv          #+#    #+#              #
-#    Updated: 2023/05/03 16:03:26 by davioliv         ###   ########.fr        #
+#    Updated: 2023/07/25 17:07:04 by davioliv         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 NAME	= libft.a
-SRCS	= ft_isdigit.c ft_isalpha.c ft_isalnum.c ft_isascii.c ft_isprint.c ft_strlen.c ft_memset.c ft_bzero.c ft_memcpy.c ft_memmove.c ft_strlcat.c ft_strlcpy.c ft_strncmp.c ft_tolower.c ft_toupper.c ft_calloc.c ft_memchr.c ft_memcmp.c ft_strchr.c ft_strdup.c ft_strnstr.c ft_strrchr.c ft_substr.c ft_atoi.c ft_strjoin.c
+SRCS	= ft_isdigit.c ft_isalpha.c ft_isalnum.c ft_isascii.c ft_isprint.c ft_strlen.c ft_memset.c ft_bzero.c ft_memcpy.c ft_memmove.c ft_strlcat.c ft_strlcpy.c ft_strncmp.c ft_tolower.c ft_toupper.c ft_calloc.c ft_memchr.c ft_memcmp.c ft_strchr.c ft_strdup.c ft_strnstr.c ft_strrchr.c ft_substr.c ft_atoi.c ft_strjoin.c ft_strtrim.c ft_split.c ft_itoa.c ft_strmapi.c ft_striteri.c ft_putchar_fd.c ft_putstr_fd.c ft_putendl_fd.c ft_putnbr_fd.c
+SRCSB	= ft_lstnew.c ft_lstadd_front.c ft_lstsize.c ft_lstlast.c ft_lstadd_back.c ft_lstdelone.c ft_lstclear.c ft_lstiter.c ft_lstmap.c
 OBJS	= ${SRCS:.c=.o}
+OBJSB	= ${SRCSB:.c=.o}
 HEADER	= libft.h
-CC	= gcc ${CFLAGS}
+CC	= cc ${CFLAGS}
 CFLAGS	= -Wall -Werror -Wextra
 INCLUDE	= -I include
-RM	= rm -f
+RM	= rm -rf
 .c.o:
 	${CC} ${INCLUDE} -c $< -o ${<:.c=.o}
 
-$(NAME):	${OBJS}
-	ar rc $(NAME) $(OBJS)
+${NAME}:	${OBJS}
+	ar rcs ${NAME} ${OBJS} ${HEADER}
 
 all:	${NAME}
 
 clean:
-	${RM} ${OBJS}
+	${RM} ${OBJS} ${OBJSB}
 
 fclean:	clean
 	${RM} ${NAME}
 
-teste:
-	${CC} ${INCLUDE} ${NAME} -o teste
+re:	fclean all
 
-re:	fclean all teste
+bonus:	${OBJS} ${OBJSB}
+	ar rcs ${NAME} ${OBJS} ${OBJSB} ${HEADER}
 
-.PHONY:	all clean fclean re
-
-
-
-
+.PHONY:	all clean fclean re bonus
--- libft.h
+++ libft.h
@@ -6,41 +6,47 @@
 /*   By: davioliv <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/17 12:47:19 by davioliv          #+#    #+#             */
-/*   Updated: 2023/05/04 10:12:17 by davioliv         ###   ########.fr       */
+/*   Updated: 2023/07/25 17:06:34 by davioliv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef	LIBFT_H
+#ifndef LIBFT_H
 # define LIBFT_H
 
 # include <unistd.h>
 # include <stdlib.h>
 # include <stdio.h>
 
+typedef struct s_list
+{
+	void			*content;
+	struct s_list	*next;
+}	t_list;
+
 void	ft_bzero(void *str, size_t n);
-int	ft_isalnum(int a);
-int	ft_isalpha(int a);
-int	ft_isascii(int a);
-int	ft_isdigit(int a);
-int	ft_isprint(int a);
+int		ft_isalnum(int a);
+int		ft_isalpha(int a);
+int		ft_isascii(int a);
+int		ft_isdigit(int a);
+int		ft_isprint(int a);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
 void	*ft_memset(void *str, int c, size_t n);
 size_t	ft_strlcpy(char *dest, const char *src, size_t size);
 size_t	ft_strlen(const char *str);
-int	ft_strncmp(const char *s1, const char *s2, size_t n);
-int	ft_tolower(int a);
-int	ft_toupper(int a);
-size_t  ft_strlcat(char *dest, const char *src, size_t size);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
+int		ft_tolower(int a);
+int		ft_toupper(int a);
+size_t	ft_strlcat(char *dest, const char *src, size_t size);
 void	*ft_calloc(size_t count, size_t size);
 void	*ft_memchr(const void *s, int c, size_t n);
-int     ft_memcmp(const void *s1, const void *s2, size_t n);
-char    *ft_strchr(const char *s, int c);
-char    *ft_strdup(const char *s1);
-char    *ft_strnstr(const char *haystack, const char *needle, size_t len);
-char    *ft_strrchr(const char *s, int c);
-char    *ft_substr(const char *s, unsigned int start, size_t len);
-int     ft_atoi(const char *str);
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strdup(const char *s1);
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
+char	*ft_strrchr(const char *s, int c);
+char	*ft_substr(const char *s, unsigned int start, size_t len);
+int		ft_atoi(const char *str);
 char	*ft_strjoin(const char *s1, const char *s2);
 char	*ft_strtrim(char const *s1, char const *set);
 char	**ft_split(char const *s, char c);
@@ -51,5 +57,14 @@ void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);
 void	ft_putendl_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
+t_list	*ft_lstnew(void *content);
+void	ft_lstadd_front(t_list **lst, t_list *new);
+int		ft_lstsize(t_list *lst);
+void	ft_lstadd_back(t_list **lst, t_list *new);
+void	ft_lstdelone(t_list *lst, void (*del)(void *));
+t_list	*ft_lstlast(t_list *lst);
+void	ft_lstclear(t_list **lst, void (*del)(void *));
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
+void	ft_lstiter(t_list *lst, void (*f)(void *));
 
 #endif
